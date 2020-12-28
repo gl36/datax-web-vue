4a2395b7b012dde8d0bfdbc8e8156f2a0ba7566c
gl36 <381219883@qq.com>
2020-12-28 14:01:32 +0800

    修改显示

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
-    <sidebar class="sidebar-container" />
-    <div :class="{hasTagsView:needTagsView}" class="main-container">
+     <div :class="{hasTagsView:needTagsView}" class="main-container">
       <div :class="{'fixed-header':fixedHeader}">
-        <navbar />
-        <tags-view v-if="needTagsView" />
-      </div>
+      <navbar />
+      <sidebar class="sidebar-container" />
       <app-main />
-      <right-panel v-if="showSettings">
-        <settings />
-      </right-panel>
+       </div>
     </div>
   </div>
 </template>
